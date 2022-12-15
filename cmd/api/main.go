b39cdef2e4a9a7176a87c90cbfdd25e2a0078bf3
Jose Cupertino Ruiz Vargas <jose.vargas@mercadolivre.com>
2022-12-14 22:51:50 -0300

    add handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
-	"nody/controler"
 	"nody/db"
-	"nody/model"
+	"nody/handler"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -17,87 +15,22 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Heartbeat("/ping"))
 
+	// never-cancelling context
+	ctx := context.Background()
+
 	// neo4j driver
 	driver := db.Driver()
-	ctx := context.Background()
 	defer driver.Close(ctx)
 
-	// create user
-	r.Post("/users", func(w http.ResponseWriter, r *http.Request) {
-		var user model.User
-		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-			panic(err)
-		}
-		if err := controler.CreateUser(ctx, driver, user); err != nil {
-			panic(err)
-		}
-		w.Write([]byte("User created"))
-	})
+	// fill the graph
+	r.Post("/users", handler.CreateUser(ctx, driver))
+	r.Post("/ipv4s", handler.CreateIPv4(ctx, driver))
+	r.Post("/hasip", handler.CreateHasIP(ctx, driver))
 
-	// create ipv4
-	r.Post("/ipv4s", func(w http.ResponseWriter, r *http.Request) {
-		var ipv4 model.IPv4
-		if err := json.NewDecoder(r.Body).Decode(&ipv4); err != nil {
-			panic(err)
-		}
-		if err := controler.CreateIPv4(ctx, driver, ipv4); err != nil {
-			panic(err)
-		}
-		w.Write([]byte("IP created"))
-	})
-
-	// create has_ip
-	r.Post("/hasip", func(w http.ResponseWriter, r *http.Request) {
-		var hasip model.HasIP
-		if err := json.NewDecoder(r.Body).Decode(&hasip); err != nil {
-			panic(err)
-		}
-		if err := controler.CreateHasIP(ctx, driver, hasip); err != nil {
-			panic(err)
-		}
-		w.Write([]byte("Relationship HAS_IP created."))
-	})
-
-	// get usernames
-	r.Get("/users/names", func(w http.ResponseWriter, r *http.Request) {
-		names, err := controler.GetUsernames(ctx, driver)
-		if err != nil {
-			panic(err)
-		}
-		response, err := json.Marshal(names)
-		if err != nil {
-			panic(err)
-		}
-		w.Write(response)
-	})
-
-	// get ips by username
-	r.Get("/users/{name}/ipv4s", func(w http.ResponseWriter, r *http.Request) {
-		name := chi.URLParam(r, "name")
-		ipv4s, err := controler.GetIPsByUsername(ctx, driver, name)
-		if err != nil {
-			panic(err)
-		}
-		response, err := json.Marshal(ipv4s)
-		if err != nil {
-			panic(err)
-		}
-		w.Write(response)
-	})
-
-	// get users by ip
-	r.Get("/ipv4s/{ip}/users", func(w http.ResponseWriter, r *http.Request) {
-		ip := chi.URLParam(r, "ip")
-		users, err := controler.GetUsersByIP(ctx, driver, ip)
-		if err != nil {
-			panic(err)
-		}
-		response, err := json.Marshal(users)
-		if err != nil {
-			panic(err)
-		}
-		w.Write(response)
-	})
+	// query the graph
+	r.Get("/users/names", handler.GetUsernames(ctx, driver))
+	r.Get("/users/{name}/ipv4s", handler.GetIPsByUsername(ctx, driver))
+	r.Get("/ipv4s/{ip}/users", handler.GetUsersByIP(ctx, driver))
 
 	http.ListenAndServe(":3000", r)
 }
---
+++ handler/handler.go
@@ -0,0 +1,95 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"nody/controler"
+	"nody/model"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+func CreateUser(ctx context.Context, driver neo4j.DriverWithContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user model.User
+		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+			panic(err)
+		}
+		if err := controler.CreateUser(ctx, driver, user); err != nil {
+			panic(err)
+		}
+		w.Write([]byte("User created"))
+	}
+}
+
+func CreateIPv4(ctx context.Context, driver neo4j.DriverWithContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var ipv4 model.IPv4
+		if err := json.NewDecoder(r.Body).Decode(&ipv4); err != nil {
+			panic(err)
+		}
+		if err := controler.CreateIPv4(ctx, driver, ipv4); err != nil {
+			panic(err)
+		}
+		w.Write([]byte("IP created"))
+	}
+}
+
+func CreateHasIP(ctx context.Context, driver neo4j.DriverWithContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var hasip model.HasIP
+		if err := json.NewDecoder(r.Body).Decode(&hasip); err != nil {
+			panic(err)
+		}
+		if err := controler.CreateHasIP(ctx, driver, hasip); err != nil {
+			panic(err)
+		}
+		w.Write([]byte("Relationship created."))
+	}
+}
+
+func GetUsernames(ctx context.Context, driver neo4j.DriverWithContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		names, err := controler.GetUsernames(ctx, driver)
+		if err != nil {
+			panic(err)
+		}
+		response, err := json.Marshal(names)
+		if err != nil {
+			panic(err)
+		}
+		w.Write(response)
+	}
+}
+
+func GetIPsByUsername(ctx context.Context, driver neo4j.DriverWithContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := chi.URLParam(r, "name")
+		ipv4s, err := controler.GetIPsByUsername(ctx, driver, name)
+		if err != nil {
+			panic(err)
+		}
+		response, err := json.Marshal(ipv4s)
+		if err != nil {
+			panic(err)
+		}
+		w.Write(response)
+	}
+}
+
+func GetUsersByIP(ctx context.Context, driver neo4j.DriverWithContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ip := chi.URLParam(r, "ip")
+		users, err := controler.GetUsersByIP(ctx, driver, ip)
+		if err != nil {
+			panic(err)
+		}
+		response, err := json.Marshal(users)
+		if err != nil {
+			panic(err)
+		}
+		w.Write(response)
+	}
+}
